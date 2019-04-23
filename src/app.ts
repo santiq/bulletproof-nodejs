d83e2e84d61459fd83531c46123795d7776c5bd1
orozcoadrian <orozcoadrian@gmail.com>
2019-04-23 07:37:36 -0700

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ async function startServer() {
   /**
    * A little hack here
    * Import/Export can only be used in 'top-level code'
-   * Well, at least in node 10 without babel and at the time of writting
+   * Well, at least in node 10 without babel and at the time of writing
    * So we are using good old require.
    **/
   await require('./loaders').default({ expressApp: app });
