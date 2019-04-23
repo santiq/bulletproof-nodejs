1dfefe021b490d52938502b4000b2af1bcb71341
Mark Railton <mark@markrailton.com>
2019-04-23 18:59:20 +0100

    adds a basic test to let circleci build

--- jest.config.js
+++ jest.config.js
@@ -117,7 +117,7 @@ module.exports = {
   // setupFiles: [],
 
   // The path to a module that runs some code to configure or set up the testing framework before each test
-  setupTestFrameworkScriptFile: './tests/setup.js',
+  // setupTestFrameworkScriptFile: './tests/setup.js',
 
   // A list of paths to snapshot serializer modules Jest should use for snapshot testing
   // snapshotSerializers: [],
---
+++ tests/sample.test.ts
@@ -0,0 +1,5 @@
+describe('Sample Test', () => {
+    it('can add 2 numbers', () => {
+        expect(1 + 2).toBe(3);
+    });
+});
