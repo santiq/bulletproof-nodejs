3fae301276d02f1f97251395224b14ea83843f47
Radomir Perišić <radeperisic@yahoo.com>
2019-05-20 22:09:32 +0200

    Remove ensure index from agenda loader

--- src/loaders/agenda.ts
+++ src/loaders/agenda.ts
@@ -1,42 +1,15 @@
 import * as Agenda from 'agenda';
 import config from '../config';
-import { Collection } from 'mongoose';
 
 export default ({ mongoConnection }) => {
-  const agenda = new Agenda() as any;
+  return new Agenda({
+    mongo: mongoConnection,
+    collection: config.agenda.dbCollection,
+    processEvery: config.agenda.pooltime,
+    maxConcurrency: config.agenda.concurrency,
+  }) as any;
   /**
    * This voodoo magic is proper from agenda.js so I'm not gonna explain too much here.
    * https://github.com/agenda/agenda#mongomongoclientinstance
    */
-  (async () => {
-    await agenda._ready;
-
-    try {
-      (agenda._collection as Collection).ensureIndex(
-        {
-          disabled: 1,
-          lockedAt: 1,
-          name: 1,
-          nextRunAt: 1,
-          priority: -1,
-        },
-        {
-          name: 'findAndLockNextJobIndex',
-        },
-      );
-    } catch (err) {
-      console.log('🔥 Failed to create Agenda index!');
-      console.log(err);
-      throw err;
-    }
-
-    console.log('✌️ Agenda index ensured');
-  })();
-
-  agenda
-    .mongo(mongoConnection, config.agenda.dbCollection)
-    .processEvery(config.agenda.pooltime)
-    .maxConcurrency(config.agenda.concurrency);
-
-  return agenda;
 };
