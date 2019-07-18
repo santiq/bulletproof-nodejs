5e1a6027b02bb9ebf9553dc76770d4b700738c2b
wangxiaoqing <free.notice.wechat@gmail.com>
2019-07-18 17:33:52 +0800

    Beautify format

--- src/api/routes/agendash.ts
+++ src/api/routes/agendash.ts
@@ -6,17 +6,18 @@ import { Container } from 'typedi'
 import config from '../../config'
 
 export default (app: Router) => {
-	const agendaInstance = Container.get('agendaInstance')
 
-	app.use('/dash', 
-		basicAuth({
-			users: {
-				[config.agendash.user]: config.agendash.password,
-			},
-			challenge: true,
-		}),
-		agendash(agendaInstance)
-	)
+  const agendaInstance = Container.get('agendaInstance')
+
+  app.use('/dash', 
+    basicAuth({
+	  users: {
+	    [config.agendash.user]: config.agendash.password,
+	  },
+	  challenge: true,
+	}),
+	agendash(agendaInstance)
+  )
 }
 
 
