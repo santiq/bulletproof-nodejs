629e6b3391daacf68f1301474a425117be7036d2
Deluxateer <deluxateer@gmail.com>
2020-05-01 17:51:31 -0400

    add more robust error detection for dotenv

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 const envFound = dotenv.config();
-if (!envFound) {
+if (envFound.error) {
   // This error should crash whole process
 
   throw new Error("⚠️  Couldn't find .env file  ⚠️");
