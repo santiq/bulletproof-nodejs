d520dc43b2d0199ebc92a996b3ae2f1d79e97444
pappyJ <peterjoshua828@gmail.com>
2021-07-26 12:46:19 +0100

    updated express.ts - OpticMiddleware is now a default export

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import { OpticMiddleware } from '@useoptic/express-middleware';
+import OpticMiddleware from '@useoptic/express-middleware';
 import routes from '../api';
 import config from '../config';
 export default ({ app }: { app: express.Application }) => {
@@ -36,9 +36,11 @@ export default ({ app }: { app: express.Application }) => {
   app.use(config.api.prefix, routes());
 
   // API Documentation
-  app.use(OpticMiddleware({
+  app.use(
+    OpticMiddleware({
       enabled: process.env.NODE_ENV !== 'production',
-  }));
+    }),
+  );
 
   /// catch 404 and forward to error handler
   app.use((req, res, next) => {
@@ -53,10 +55,7 @@ export default ({ app }: { app: express.Application }) => {
      * Handle 401 thrown by express-jwt library
      */
     if (err.name === 'UnauthorizedError') {
-      return res
-        .status(err.status)
-        .send({ message: err.message })
-        .end();
+      return res.status(err.status).send({ message: err.message }).end();
     }
     return next(err);
   });
